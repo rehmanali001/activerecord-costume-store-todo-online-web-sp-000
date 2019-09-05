a01ceb383df2e679a02fdeaad8c161ef0ddde68d
Rehman Ali <rehmanali001@gmail.com>
2019-09-05 20:11:38 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,16 +1,5 @@
 # Create your haunted_houses migration here
 
 class CreateHauntedHouses < ActiveRecord::Migration[5.2]
-  def change 
-    create_table :haunted_houses do |t|
-      t.string :name 
-      t.location :string 
-      t.string :theme
-      t.integer :price 
-      t.boolean :friendly_or_not
-      t.datetime :opening_date
-      t.datetime :closing_date
-      t.text :long_description
-    end
-  end 
+ 
 end
