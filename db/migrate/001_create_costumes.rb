75bf271580a5849ce6731def7a2a0dd49235e3f1
Rehman Ali <rehmanali001@gmail.com>
2019-09-05 19:55:37 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -16,8 +16,8 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
       t.integer :price
       t.integer :size 
       t.string :image_url
-      t.timestamps :created_at 
-      t.timestamps :updated_at
+      t.timestamps
+
     end
   end 
 end
