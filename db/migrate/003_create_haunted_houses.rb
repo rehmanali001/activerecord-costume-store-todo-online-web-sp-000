2e3f46cbcb43bbbfe7e7d45fd87e68beda815126
Rehman Ali <rehmanali001@gmail.com>
2019-09-05 20:13:06 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,7 +7,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.string :location 
       t.string :theme
       t.integer :price 
-      t.boolean :friendly_or_not
+      t.boolean :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
       t.text :long_description
