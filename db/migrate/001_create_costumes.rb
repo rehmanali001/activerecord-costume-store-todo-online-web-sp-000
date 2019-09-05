0183e6982935d100a88e486aa7222cfefdcf2e87
Rehman Ali <rehmanali001@gmail.com>
2019-09-05 19:46:32 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,6 +12,8 @@ class Costume < ActiveRecord::Migration[5.2]
   
   def change 
     create_table :costumes do |t|
-      t.
+      t.string :name 
+      t.integer :price
+      t.integer :size
   end 
 end
